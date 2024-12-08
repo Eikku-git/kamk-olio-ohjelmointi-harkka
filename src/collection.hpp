fd74dad3f430a333352fc0ab46921770a206c6a4
Einari Mäkiranta <makirantaeinari@gmail.com>
2024-12-09 00:30:31 +0200

    Modified Collection class

--- src/collection.hpp
+++ src/collection.hpp
@@ -14,21 +14,33 @@ public:
 
 private:
 	const char* const c_Name;
-	const char* const c_ItemName;
 	std::vector<Item> m_Items;
 public:
 
-	Collection(const char* colName, const char* itemName) : c_Name(colName), c_ItemName(itemName), m_Items() {
-		std::ifstream filestream((std::string)c_name + ".col");
+	bool Save() {
+		std::ofstream filestream((std::string)c_Name + ".col", std::ofstream::out | std::ofstream::trunc);
+		if (!filestream.is_open() || !filestream.good()) {
+			return false;
+		}
+		for (const Item& item : m_Items) {
+			filestream << item;	
+		}
+		return true;
+	}
+
+	bool Load() {
+		std::ifstream filestream((std::string)c_Name + ".col");
 		if (filestream.is_open()) {
 			if (!filestream.good()) {
 				std::cout << "failed to open games file!\n";
+				return false;
 			} 
 			while (filestream.good() && filestream.peek() != EOF) {
 				filestream >> m_Items.emplace_back();
 				filestream.get();
 			}
 		}
+		return true;
 	}
 
 	const std::vector<Item>& GetItems() {
@@ -48,38 +60,79 @@ public:
 		return true;
 	}
 
+	bool EditItem(size_t index) {
+		if (index >= m_Items.size()) {
+			std::cout << "Invalid index!";
+			return false;
+		}
+		m_Items[index].Edit();
+	}
+
 	void ViewMenu() {
-		std::cout << "Welcome to " << c_Name << " collection\n" << "Select an option\n"
-			<< "(1) View items"
-			<< "(2) Add item"
-			<< "(3) Remove item";
-		int opt;
-		std::cin >> opt;
-		switch (opt) {
-		case 1:
-			size_t count = m_Items.size();
-			for (size_t i = 0; i < count; i++) {
-				std::cout << i + 1 << '\n'
-				m_Items[i].Print();
+		std::cout << "Welcome to " << c_Name << " collection" << std::endl;
+		while (true) {
+			std::cout << "Select an option" << std::endl
+				<< "(1) View items" << std::endl
+				<< "(2) Add item" << std::endl
+				<< "(3) Edit item" << std::endl
+				<< "(4) Remove item" << std::endl
+				<< "(5) Return to main menu" << std::endl;
+			int opt;
+			std::cin >> opt;
+			switch (opt) {
+				case 1: {
+					size_t count = m_Items.size();
+					for (size_t i = 0; i < count; i++) {
+						std::cout << i + 1 << '\n';
+						m_Items[i].Print();
+					}
+					break;
+				}
+				case 2: {
+					Item& item = m_Items.emplace_back();
+					if (!item.FillInfo()) {
+						m_Items.pop_back();
+					}
+					break;
+				}
+				case 3: {
+					std::cout << "Type an index or -1 to cancel" << std::endl;
+					std::cin >> opt;
+					if (opt == -1) {
+						break;
+					}
+					--opt;
+					EditItem(opt);
+					break;
+				}
+				case 4: {
+					std::cout << "Type an index or -1 to cancel" << std::endl;
+					std::cin >> opt;
+					if (opt == -1) {
+						break;
+					}
+					if (!DeleteItem(opt - 1)) {
+						std::cout << "Couldn't delete item!" << std::endl;
+					}
+					break;
+				}
+				case 5:
+					return;
+				default:
+					std::cout << "Invalid option!" << std::endl;
 			}
-			break;
-		case 2:
-			Item item{};
-			if (Item::Create(item)) {
-				m_Items.emplace_back(std::move(item));
-			}
-		case 3:
-			std::cout << "Type an index";
-			std::cin >> opt--;
-			DeleteItem(opt);
-			break;
+		}
+	}
+
+	Collection(const char* colName) : c_Name(colName), m_Items() {
+		if (!Load()) {
+			std::cout << "failed to load" << c_Name << " collection" << std::endl;
 		}
 	}
 
 	~Collection() {
-		std::ofstream filestream((std::string)c_name + ".col", std::ofstream::out | std::ofstream::trunc);
-		for (const Item& item : m_Items) {
-			filestream << item;	
+		if (!Save()) {
+			std::cout << "failed to save " << c_Name << " collection" << std::endl;
 		}
 	}
 };
