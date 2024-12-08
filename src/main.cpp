477d216d57121fef5a1e60ffdcdceaab76a80a33
Einari Mäkiranta <makirantaeinari@gmail.com>
2024-12-09 00:30:46 +0200

    Finished main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,17 @@ int main() {
 	bool running = true;
 	while (running) {
 		std::cout << "Select an option\n" 
-			<< "(1) Go to game collection\n"
-			<< "(2) Go to book collection\n"
+			<< "(1) Go to games collection\n"
+			<< "(2) Go to books collection\n"
 			<< "(3) End program\n";
 		int opt;
 		std::cin >> opt;
 		switch (opt) {
 		case 1:
+			games.ViewMenu();
 			break;
 		case 2:
+			books.ViewMenu();
 			break;
 		case 3:
 			running = false;
